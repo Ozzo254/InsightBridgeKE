e3ef5f020286cfb852d779a3cf521770358144d5
ozzo_254 <veetruth99@gmail.com>
2024-08-23 06:46:18 -0400

    ibk

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,40 @@
-import { useState } from "react";
+import { useEffect, /* useRef, */ useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Header, { HeaderPhone } from "./components/Header";
 import Form from "./components/Form";
 import Hero from "./components/Hero";
 
+import Lenis from "lenis";
+// import { useScroll } from "framer-motion";
+
 const App = () => {
+  // const container = useRef();
+  /*   const scrollYPogress = useScroll({
+    target: container,
+    offset: ["start start", "end end"],
+  }); */
+
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
+
   const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <>
+      {/* <main  ref={container} > */}
       <HeaderPhone menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <Header menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-      <Hero />
-
+      <Hero /* scrollYPogress={scrollYPogress} */ />
+      {/* </main> */}
       <Router>
         <Routes>
           <Route path="/form" element={<Form />} />
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,40 @@
-import { animate, motion } from "framer-motion";
+import PropTypes from "prop-types";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
 import Typewriter from "typewriter-effect";
+
 const Hero = () => {
+  const container = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ["start start", "end end"],
+  });
+
+  return (
+    <div className="container-hero" ref={container} id="home">
+      <Banner scrollYProgress={scrollYProgress} />
+      <Section scrollYProgress={scrollYProgress} />
+    </div>
+  );
+};
+const Section = ({ scrollYProgress }) => {
+  const rotate = useTransform(scrollYProgress, [0, 1], [-10, 0]);
+
   const animations = {
     h2: {
       initial: { opacity: 0.1 },
       whileInView: { opacity: 1 },
     },
   };
-  return (
-    <section className="hero">
-      <div className="hero-overlay">
-        <motion.h2 {...animations.h2}>
-          Innovate Boldly, Secure Confidently
-        </motion.h2>
 
+  return (
+    <motion.section style={{ rotate }} className="hero">
+      <div className="hero-overlay">
+        <motion.h2 {...animations.h2} className="h2">
+          Innovate Boldly, <br />
+          Secure Confidently
+        </motion.h2>
+        |{" "}
         <Typewriter
           options={{
             strings: [
@@ -25,12 +46,32 @@ const Hero = () => {
             autoStart: true,
             loop: true,
             cursor: "",
-            wrapperClassName: "typewritterpara",
+            wrapperClassName: "typewriterpara",
           }}
+          className="typewriter"
         />
       </div>
-    </section>
+    </motion.section>
   );
+};
+
+const Banner = ({ scrollYProgress }) => {
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 0.8]);
+  const rotate = useTransform(scrollYProgress, [0, 1], [0, -10]);
+
+  return (
+    <motion.div style={{ scale, rotate }} className="banner">
+      <h1>I B K</h1>
+    </motion.div>
+  );
+};
+
+Banner.propTypes = {
+  scrollYProgress: PropTypes.object.isRequired,
+};
+
+Section.propTypes = {
+  scrollYProgress: PropTypes.object.isRequired,
 };
 
 export default Hero;
