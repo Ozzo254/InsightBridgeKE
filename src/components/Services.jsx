7b2d286b7e30e21e26dcf379a8fd5b01dc1229da
ozzo_254 <veetruth99@gmail.com>
2024-08-25 04:12:47 -0400

    ibk

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,5 +1,12 @@
 const Services = () => {
-  return <section>Services</section>;
+  return (
+    <>
+      <section className="empty"></section>
+      <section className="mainSection">
+        <h1>Our Services</h1>
+      </section>
+    </>
+  );
 };
 
 export default Services;
